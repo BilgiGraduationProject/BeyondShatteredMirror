20f19f95447ad9212dc26cbd3e601a266ccc8c59
dgdfurkan <frkngndz60@gmail.com>
2024-01-10 23:14:13 +0300

    InventoryPanel edited
    Signed-off-by: dgdfurkan <frkngndz60@gmail.com>

--- Assets/Scripts/Runtime/Controllers/UI/InventoryPanelController.cs
+++ Assets/Scripts/Runtime/Controllers/UI/InventoryPanelController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
+using Runtime.Data.ValueObject;
 using UnityEngine;
 using UnityEngine.UI;
+using Runtime.Managers;
+using Runtime.Utilities;
+using TMPro;
 
 namespace Runtime.Controllers.UI
 {
@@ -10,15 +14,41 @@ namespace Runtime.Controllers.UI
         
         #region Serialized Variables
 
-        [SerializeField] private GameObject photoObjectParent;
-        [SerializeField] private GameObject photoObjectPrefab;
+        [Header("User Area Settings")]
+        [SerializeField] private Image userAvatarImage;
+        [SerializeField] private TextMeshProUGUI userNameText;
+        
+        [Header("Health Area Settings")]
+        [SerializeField] private GameObject healthImage;
+        
+        [Header("Weapon Area Settings")]
+        [SerializeField] private Image weaponImage;
+        [SerializeField] private TextMeshProUGUI weaponNameText;
+        
+        [Header("Selected Area Settings")]
+        [SerializeField] private Image selectedImage;
+        
+        [Header("Description Area Settings")]
+        [SerializeField] private TextMeshProUGUI descriptionText;
+        
+        [Header("Items Area Settings")]
+        [Header("Item Objects Area Settings")]
+        [SerializeField] private Transform itemObjectParent;
+        [SerializeField] private GameObject itemObjectPrefab;
+        
+        [Header("Capture Objects Area Settings")]
+        [SerializeField] private Transform captureObjectParent;
+        [SerializeField] private GameObject captureObjectPrefab;
         
         #endregion
         
         #region Private Variables
 
-        private Texture2D[] _photoTextureArray;
-        private List<Texture2D> _photoTextureList = new List<Texture2D>();
+        private Texture2D[] _pcaptureTextureArray;
+        private List<Texture2D> _captureData = new List<Texture2D>();
+        private List<ItemData> _itemData = new List<ItemData>();
+        
+        private List<GameObject> _itemObjects = new List<GameObject>();
         
         #endregion
 
@@ -27,32 +57,79 @@ namespace Runtime.Controllers.UI
         private void Awake()
         {
             //if(_photoSpriteList.Count > 0) _photoSpriteList.Clear();
-            AddPhotosToPanel();
+            GetItemDatas();
+            GetCaptureDatas();
         }
 
         private void Start()
         {
-            CreatePhotoObjects();
+            GetItemObjects();
+            GetCaptureObjects();
         }
 
-        private void AddPhotosToPanel()
+        private void GetItemDatas()
         {
-            _photoTextureArray = Resources.LoadAll<Texture2D>("CapturePhotos");
+            _itemData = ShopManager.Instance.SendItemDatasToControllers();
+        }
+        
+        private void GetCaptureDatas()
+        {
+            _pcaptureTextureArray = Resources.LoadAll<Texture2D>("CapturePhotos");
 
-            foreach (var photo in _photoTextureArray)
+            foreach (var photo in _pcaptureTextureArray)
             {
-                _photoTextureList.Add(photo);
+                _captureData.Add(photo);
             }
         }
-
-        private void CreatePhotoObjects()
+        
+        private void GetItemObjects()
         {
-            foreach (var photo in _photoTextureList)
+            foreach (var item in _itemData)
             {
-                GameObject photoObject = Instantiate(photoObjectPrefab, photoObjectParent.transform);
+                if (ES3.KeyExists(item.Name))
+                {
+                    print(item.Name + " : " + SaveLoadManager.Instance.LoadData<int>(item.Name));
+                    GameObject itemObject = Instantiate(itemObjectPrefab, itemObjectParent);
+                    _itemObjects.Add(itemObject);
+                    itemObject.GetComponent<Toggle>().onValueChanged.AddListener((value) =>
+                    {
+                        DisableItemsToggle();
+                        if (value)
+                        {
+                            itemObject.transform.GetChild(0).GetComponent<Outline>().enabled = true;
+                            selectedImage.sprite = item.Thumbnail;
+                            descriptionText.text = item.Description;
+                        }
+                    });
+                    //itemObject.gameObject.GetComponent<Toggle>().onValueChanged.AddListener((value => DisableItemsToggle()));
+                    itemObject.transform.GetChild(1).gameObject.GetComponent<Image>().sprite = item.Thumbnail;
+                    itemObject.transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>().text = SaveLoadManager.Instance.LoadData<int>(item.Name).ToString();
+                }
+            }
+            
+            //Selected first one
+            _itemObjects[0].transform.GetChild(0).GetComponent<Outline>().enabled = true;
+            selectedImage.sprite = _itemData[0].Thumbnail;
+            descriptionText.text = _itemData[0].Description;
+        }
+
+        private void GetCaptureObjects()
+        {
+            foreach (var photo in _captureData)
+            {
+                GameObject photoObject = Instantiate(captureObjectPrefab, captureObjectParent);
                 //photoObject.GetComponent<Image>().sprite = photo;
                 photoObject.transform.GetChild(0).transform.GetChild(0).GetComponent<RawImage>().texture = photo;
             }
+        }
+
+        private void DisableItemsToggle()
+        {
+            _itemObjects.ForEach(item =>
+            {
+                item.transform.GetChild(0).GetComponent<Outline>().enabled = false;
+                item.GetComponent<Toggle>().isOn = false;
+            });
         }
     }
 }
